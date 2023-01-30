289ff8cfe59491eb2c42736d081b36c09b82e3ed
Zeler Villarreal <villarreal.benji@gmail.com>
2023-01-30 16:48:55 +0000

    Update install_packages.sh

--- run_once_install_packages.sh
+++ run_once_install_packages.sh
@@ -4,8 +4,6 @@
 brew install git
 
 #Install JDk MAN
-curl -s "https://get.sdkman.io" | bash
-
-source "$HOME/.sdkman/bin/sdkman-init.sh"
+brew install unzip & curl -s "https://get.sdkman.io" | bash & source "$HOME/.sdkman/bin/sdkman-init.sh"
 
 #Install Maven
