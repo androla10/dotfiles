d5fbc1d3b6c2de16969d5b455708c6e2177e077b
Zeler Benji Villarreal Marcelo <villarreal.benji@gmail.com>
2024-07-15 03:31:22 -0500

    Enable install brew git and export in path

--- run_once_install_packages.sh
+++ run_once_install_packages.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+export PATH="$HOME/.linuxbrew/bin:$PATH"
+
 #Install Git
-#brew install git
+brew install git
 
 #Install JDk MAN
 #brew install unzip & curl -s "https://get.sdkman.io" | bash & source "$HOME/.sdkman/bin/sdkman-init.sh"
