3b71d34e81c37f22839b3063e4ec1aae1fdb2fd1
Zeler Benji Villarreal Marcelo <villarreal.benji@gmail.com>
2024-08-19 18:31:24 -0500

    add script for execute script

--- run_once_install_packages.sh
+++ run_once_install_packages.sh
@@ -2,10 +2,8 @@
 
 export PATH="$HOME/.linuxbrew/bin:$PATH"
 
-#Install Git
-brew install git
+#Install from Brewfile
+brew bundle --file=~/.private/Brewfile
 
 #Install JDk MAN
 #brew install unzip & curl -s "https://get.sdkman.io" | bash & source "$HOME/.sdkman/bin/sdkman-init.sh"
-
-#Install Maven
