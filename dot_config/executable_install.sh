e6d3d54ad47fe7b1d8604d13b9b9d8202ff0d6de
Zeler Benji Villarreal Marcelo <villarreal.benji@gmail.com>
2024-09-19 18:49:32 -0500

    add install and functions sh

---
+++ dot_config/executable_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Autor Benji Villarreal
+#This program allows the installation of all the programs neecessary for my setup. 
+
+#Configuration Environtment Variables
+PATH_CONFIG="$HOME/.config"
+
+#Install SDKMAN
+#curl -s "https://get.sdkman.io" | bash
+
+source "$PATH_CONFIG/.functions.sh"
