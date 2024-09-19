0d82e1f9d52d3328c9b1f2b93f243058369b89fa
Zeler Benji Villarreal Marcelo <villarreal.benji@gmail.com>
2024-09-19 18:53:21 -0500

    add install zap plugin manager

--- run_once_install_packages.sh
+++ run_once_install_packages.sh
@@ -7,3 +7,7 @@ brew bundle --file=~/.private/Brewfile
 
 #Install JDk MAN
 #brew install unzip & curl -s "https://get.sdkman.io" | bash & source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+#Install Zap Plugin Manager
+zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+
