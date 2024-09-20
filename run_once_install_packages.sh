5cee26ed1c0cd8ea3130fa7fc68b52a8b6daeb24
Zeler Benji Villarreal Marcelo <villarreal.benji@gmail.com>
2024-09-19 23:22:31 -0500

    enable install sh

--- run_once_install_packages.sh
+++ run_once_install_packages.sh
@@ -6,14 +6,14 @@ export PATH="$HOME/.linuxbrew/bin:$PATH"
 brew bundle --file=~/.private/Brewfile
 
 #Execute change other shell
-echo $(which zsh) | sudo tee -a /etc/shells
-chsh -s $(which zsh) $USER
+#echo $(which zsh) | sudo tee -a /etc/shells
+#chsh -s $(which zsh) $USER
 
 #Execute Script Install other applications
-#$HOME/.config/install.sh
+$HOME/.config/install.sh
 
 
 #Install Zap Plugin Manager
-zsh --version
+#zsh --version
 #zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
 
