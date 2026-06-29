9e259bc778d309bb10a691f94217ded006a9daa8
Zeler Benji Villarreal Marcelo <villarreal.benji@gmail.com>
2026-06-28 19:35:06 -0500

    feat: add nvchad in install packages

--- run_once_install_packages_windows.ps1
+++ run_once_install_packages_windows.ps1
@@ -1,5 +1,5 @@
-# Instalar git curl neovim chezmoi
-winget install --id=Git.Git -e --silent --source winget
-
 # Install NeoVim
 winget install -e --id Neovim.Neovim --source winget
+
+# Instalamos NvChad
+git clone https://github.com/NvChad/starter $ENV:USERPROFILE\AppData\Local\nvim; nvim
