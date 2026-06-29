d61d80ff26cf5e6283162f210c88e9743db5cd46
Zeler Benji Villarreal Marcelo <villarreal.benji@gmail.com>
2026-06-29 01:46:18 -0500

    feat: add new nerd fonts

--- run_once_install_packages_windows.ps1
+++ run_once_install_packages_windows.ps1
@@ -1,5 +1,8 @@
 # Install NeoVim
 winget install -e --id Neovim.Neovim --source winget
 
+# Install Nerd Fonts
+winget install -e --id DEVCOM.JetBrainsMonoNerdFont --source winget
+
 # Instalamos NvChad
 git clone https://github.com/NvChad/starter $ENV:USERPROFILE\AppData\Local\nvim; nvim
